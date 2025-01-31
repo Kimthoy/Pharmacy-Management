1f33597d0d710629bd305ecf6aebe22b3afd17e1
Kimthoy <pheantiger03@gmail.com>
2025-01-31 14:48:36 +0700

    added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,235 +1,125 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import {
   HomeIcon,
   ClipboardDocumentListIcon,
   Cog6ToothIcon,
-  // CurrencyRupeeIcon,
   BellIcon,
-  Bars3Icon,
-  XMarkIcon,
   FolderIcon,
-  PowerIcon,
   FlagIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
   ShoppingBagIcon,
-  BanknotesIcon,
-  InformationCircleIcon,
-  CalculatorIcon,
   CalendarDaysIcon,
+  BanknotesIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { CurrencyDollarIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronDoubleRightIcon,
+  ChevronDoubleLeftIcon,
+} from "@heroicons/react/24/outline";
 
 const Sidebar = ({ setSelectedPage, selectedPage }) => {
-  const [isSvgChanged, setIsSvgChanged] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedMenuItem, setSelectedMenuItem] = useState("Dashboard");
-  const [setIsInventoryOpen] = useState(false);
-  const [isSidebarMinimized, setIsSidebarMinimized] = useState(false);
-  const buttonRef = useRef(null);
-  const menuRef = useRef(null);
   const navigate = useNavigate();
-  const menuItems = React.useMemo(
-    () => [
-      { name: "Dashboard", icon: HomeIcon, path: "/" },
-      { name: "Inventory", icon: ShoppingBagIcon, path: "/inventory" },
-      {
-        name: "Stock",
-        icon: ClipboardDocumentListIcon,
-        path: "/list-of-medicine",
-      },
-      {
-        name: "Medicine Group",
-        icon: FolderIcon,
-        path: "/medicine-group",
-      },
 
-      {
-        name: "Expense",
-        icon: CurrencyDollarIcon,
-        path: "/money-mgt",
-      },
-      { name: "Daily Income", icon: BanknotesIcon, path: "/dailyincome" },
-      {
-        name: "Monthly Income",
-        icon: CalendarDaysIcon,
-        path: "/monthlyincome",
-      },
-      { name: "Notifications", icon: BellIcon, path: "/notifications" },
-      { name: "Report", icon: FlagIcon, path: "/report" },
-      { name: "Setting", icon: Cog6ToothIcon, path: "/configuration" },
-    ],
-    []
-  );
+  // State to track the visibility of the sub-sidebar
+  const [isSubSidebarOpen, setIsSubSidebarOpen] = useState(false);
 
-  const toggleSvg = () => setIsSvgChanged((prev) => !prev);
-  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
-
-  const handleMenuItemClick = (item) => {
-    setSelectedMenuItem(item);
-    setIsMenuOpen(false);
-    if (item === "Profile" || item === "Logout") {
-      setIsSvgChanged(false);
-    }
-  };
+  const menuItems = [
+    { name: "Dashboard", icon: HomeIcon, path: "/" },
+    { name: "Inventory", icon: ShoppingBagIcon, path: "/inventory" },
+    {
+      name: "Stock",
+      icon: ClipboardDocumentListIcon,
+      path: "/list-of-medicine",
+    },
+    { name: "Medicine Group", icon: FolderIcon, path: "/medicine-group" },
+    { name: "Expense", icon: CurrencyDollarIcon, path: "/money-mgt" },
+    { name: "Daily Income", icon: BanknotesIcon, path: "/dailyincome" },
+    { name: "Monthly Income", icon: CalendarDaysIcon, path: "/monthlyincome" },
+    { name: "Notifications", icon: BellIcon, path: "/notifications" },
+    { name: "Report", icon: FlagIcon, path: "/report" },
+    { name: "Setting", icon: Cog6ToothIcon, path: "/configuration" },
+  ];
 
   const handlePageSelection = (item, path) => {
-    setSelectedMenuItem(item);
     setSelectedPage(item);
     navigate(path);
   };
 
-  const handleInventoryToggle = () => {
-    setIsInventoryOpen((prev) => !prev);
+  const toggleSubSidebar = () => {
+    setIsSubSidebarOpen(!isSubSidebarOpen); // Toggle the sub-sidebar visibility
   };
-
-  const toggleSidebar = () => {
-    setIsSidebarMinimized((prev) => !prev);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (
-        menuRef.current &&
-        !menuRef.current.contains(event.target) &&
-        buttonRef.current &&
-        !buttonRef.current.contains(event.target)
-      ) {
-        setIsMenuOpen(false);
-        setIsSvgChanged(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   return (
     <div className="flex">
-      <div
-        className={`${
-          isSidebarMinimized ? "w-15" : "w-62"
-        } bg-gray-100 text-black flex flex-col shadow-md justify-between h-screen p-1 transition-all duration-300 relative `}
-      >
-        <div>
-          {/* Top Section with User Profile */}
-          <div className="flex items-center mb-6 relative">
-            <img
-              src="logo.png"
-              alt="User"
-              className="w-12 rounded-full mr-4 shadow-md"
-            />
-            {!isSidebarMinimized && (
-              <>
-                <div>
-                  <h3 id="admin_name" className="font-thin  font-serif">
-                    KHOY ROTHANAK
-                  </h3>
-                  <p
-                    id="name_phamacy"
-                    className="font-serif font-thin text-blue-300"
-                  >
-                    PHANHARITH PHAMACY
-                  </p>
-                </div>
-              </>
-            )}
-            {!isSidebarMinimized && (
-              <>
-                <button
-                  ref={buttonRef}
-                  className="ml-auto text-black  hover:text-gray-300"
-                  onClick={() => {
-                    toggleSvg();
-                    toggleMenu();
-                  }}
-                >
-                  {!isSvgChanged ? (
-                    <Bars3Icon className="h-3 w-3" />
-                  ) : (
-                    <XMarkIcon className="h-3 w-3" />
-                  )}
-                </button>
-              </>
-            )}
-            {isMenuOpen && (
-              <div
-                ref={menuRef}
-                className="absolute right-0 top-12 w-36 bg-white rounded shadow-lg"
-              >
-                <ul>
-                  <li
-                    onClick={() => handleMenuItemClick("Profile")}
-                    className={`p-2 hover:bg-gray-200 rounded cursor-pointer text-blue-500 ${
-                      selectedMenuItem === "Profile" ? "bg-white-100" : ""
-                    }`}
-                  >
-                    <div className="flex justify-around">
-                      <HomeIcon className="h-6 w-6" />
-                      <span>My Profile</span>
-                    </div>
-                  </li>
-                  <hr />
-                  <li
-                    onClick={() => handleMenuItemClick("Logout")}
-                    className={`p-2 hover:bg-gray-200 rounded cursor-pointer text-red-500 ${
-                      selectedMenuItem === "Logout" ? "bg-white-100" : ""
-                    }`}
-                  >
-                    <div className="flex justify-around">
-                      <PowerIcon className="h-6 w-6" />
-                      <span>Logout</span>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
+      {/* Main Sidebar */}
+      <div className="w-16 bg-gray-100 text-black flex flex-col shadow-md justify-between h-screen relative">
+        <img src="images/logo.png" className="mb-5" alt="Logo" />
+        <hr />
+        <ul className="space-y-2">
+          {menuItems.map(({ name, icon: Icon, path }) => (
+            <li
+              key={name}
+              onClick={() => handlePageSelection(name, path)}
+              className={`relative p-2 rounded cursor-pointer flex items-center justify-center hover:bg-gray-300 group transition-all duration-300 ease-in-out transform ${
+                selectedPage === name ? "bg-blue-200 text-blue-600" : ""
+              }`}
+            >
+              <Icon className="h-6 w-6" />
+            </li>
+          ))}
 
-          {/* Menu Items */}
-          <ul className="space-y-1">
-            {menuItems.map(({ name, icon: Icon, path }) => (
-              <React.Fragment key={name}>
-                <li
-                  onClick={() =>
-                    name === ""
-                      ? handleInventoryToggle()
-                      : handlePageSelection(name, path)
-                  }
-                  className={`p-2 rounded cursor-pointer flex items-center hover:bg-blue-500 ${
-                    selectedMenuItem === name ? "bg-blue-500 text-white" : ""
-                  }`}
-                >
-                  <Icon className="h-6 w-6 inline mr-2" />
-                  {!isSidebarMinimized && name}
-                  {name === "" && !isSidebarMinimized}
-                </li>
-              </React.Fragment>
-            ))}
+          {/* Button to toggle sub-sidebar */}
+          <div className="absolute top-32 left-11 w-10 h-10 rounded-full shadow-md bg-gray-200">
+            <button
+              onClick={toggleSubSidebar}
+              className="relative p-2 cursor-pointer flex items-center justify-center transform"
+            >
+              {isSubSidebarOpen ? (
+                <ChevronDoubleLeftIcon className="h-6 w-6" />
+              ) : (
+                <ChevronDoubleRightIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </ul>
+      </div>
+
+      {/* Sub-sidebar (Pop-up) */}
+      {isSubSidebarOpen && (
+        <div
+          className="w-40 bg-gray-200 text-black flex flex-col shadow-md justify-between h-screen p-4 left-0 top-0 transition-all duration-300 ease-in-out"
+          onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside sub-sidebar
+        >
+          <button className="text-red-500 " onClick={toggleSubSidebar}>
+            <XMarkIcon className="w-5 h-5 ml-52" />
+          </button>
+          <ul className=" mb-96">
+            {/* Sub-sidebar items with labels only (no icons) */}
+            <li
+              onClick={() => handlePageSelection("Sub-Stock", "/sub-stock")}
+              className="p-3  rounded cursor-pointer hover:bg-gray-300 transition-all duration-300 ease-in-out"
+            >
+              Sub-Stock
+            </li>
+            <li
+              onClick={() =>
+                handlePageSelection("Sub-Medicine", "/sub-medicine")
+              }
+              className="p-3  rounded cursor-pointer hover:bg-gray-300 transition-all duration-300 ease-in-out"
+            >
+              Sub-Medicine
+            </li>
+            <li
+              onClick={() => handlePageSelection("Sub-Expense", "/sub-expense")}
+              className="p-3  rounded cursor-pointer hover:bg-gray-300 transition-all duration-300 ease-in-out"
+            >
+              Sub-Expense
+            </li>
           </ul>
         </div>
-        <div className="mt-auto">
-          <button
-            onClick={toggleSidebar}
-            className="bg-gray-700 text-white w-10 p-2 rounded flex items-center justify-end hover:bg-gray-600"
-          >
-            {isSidebarMinimized ? (
-              <ChevronRightIcon className="h-6 w-6" />
-            ) : (
-              <ChevronLeftIcon className="h-6 w-6" />
-            )}
-          </button>
-        </div>
-        {/* <div className="absolute top-20 left-28 bg-gray-300 rounded-full shadow-slate-50000 w-9 h-9 flex justify-center align-middle">
-          <button>
-            <ChevronRightIcon className="h-6 w-6" />
-          </button>
-        </div> */}
-      </div>
+      )}
     </div>
   );
 };
